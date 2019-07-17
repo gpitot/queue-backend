2e9f8a29230e7633e2b90d7a8e62e39eccdacc1d
Guillaume Pitot <gpitot@nine.com.au>
2019-07-17 16:09:15 +1000

    add and add custom

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,18 +1,39 @@
 var router = require('express').Router()
 var auth = require('../auth');
-
+var {
+    validateJson
+} = require('./validation');
 
 var {
-    createTable,
-    removeUser,
-    addUserToQueue,
-    getUserPosition
+    query
 } = require('../../database');
 
 //adds user to queue
 router.post(`/add`, (req, res, next) => {
-
+    if (!validateJson(req.body, ['user'])) {
+        res.sendStatus(400).json({success:false,message:'missing user field'})
+    }
+    const {user} = req.body;
+    const validate = validateJson(user, ['name', 'phone', 'genderPreference', 'doctor']);
+    if (validate !== true) {
+        res.sendStatus(400).json({success:false, message:`missing ${validate} field`})
+    }
+    const {name, phone, genderPreference, doctor}
+    const sql = `INSERT INTO QUEUE (name, phone, genderPreference, dr_id) VALUES ($1, $2, $3, $4)`;
+    const values = [name, phone, genderPreference, doctor];
+    query(sql, values).then((result) => {
+        res.json(result);
+    });
 });
+
+
+router.post('/add_custom', (req, res, next) => {
+    const sql = req.body.sql;
+    const values = req.body.values;
+    query(sql, values).then((result) => {
+        res.json(result);
+    });
+})
 
 //removes user from queue, (success or failure) (client showed up or not)
 router.post(`/remove`, (req, res, next) => {
@@ -25,13 +46,13 @@ router.get(`/queue`, auth.required, (req, res, next) => {
 });
 
 
-router.post(`/create`, (req, res, next) => {
+// router.post(`/create`, (req, res, next) => {
 
-    createTable(req.body.sql, (result) => {
-        res.json(result);
-    });
+//     createTable(req.body.sql, (result) => {
+//         res.json(result);
+//     });
     
-});
+// });
 
 
 
